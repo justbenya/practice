f64a9c77ee8c0660cf96f627dccc02361c4867cd
Just Benya <justbenya@gmail.com>
2017-08-04 17:58:49 +0700

    Реализовал сохранение файла.

--- src/com/practice/File.java
+++ src/com/practice/File.java
@@ -4,31 +4,30 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class File {
-    private static final java.io.File URL = new java.io.File("file1.txt");
     private ArrayList<String> list = new ArrayList<>();
+    private static final java.io.File INPUT_FILE = new java.io.File("in.txt");
+    private static final java.io.File OUTPUT_FILE = new java.io.File("out.txt");
 
     public ArrayList<String> getList() {
         return list;
     }
 
-    public void loadFile() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(URL));
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE))) {
             String line;
-            while ( (line = reader.readLine() ) != null) {
+            while ((line = reader.readLine()) != null) {
                 list.add(line);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        File file = new File();
-        file.loadFile();
-        System.out.println(file.getList());
-
+    public void save(String out) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE))) {
+            writer.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
