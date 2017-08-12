b88e66048bf7ca9f05c69e7c33d0bbf5447fcc42
Just Benya <justbenya@gmail.com>
2017-08-12 10:41:49 +0700

    Реализация класса для сортировки.

--- src/main/java/com/practice/Sort.java
+++ src/main/java/com/practice/Sort.java
@@ -1,7 +1,12 @@
 package com.practice;
 
+/**
+ * Класс для сортировки целых чисел по возрастанию, убыванию
+ * для сортировки строк по возрастанию, убыванию.
+ */
 public class Sort {
-    public int[] insertionSort(int[] array) {
+
+    public int[] insertionSortIncrease(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int j;
             int buf = array[i];
@@ -18,13 +23,47 @@ public class Sort {
         return array;
     }
 
-    public String[] insertionSort(String[] array) {
+    public int[] insertionSortDecrease(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int j;
+            int buf = array[i];
+
+            for (j = i - 1; j >= 0; j--) {
+                if (array[j] > buf) {
+                    break;
+                }
+                array[j + 1] = array[j];
+            }
+
+            array[j + 1] = buf;
+        }
+        return array;
+    }
+
+    public String[] insertionSortIncrease(String[] array) {
         for (int i = 1; i < array.length; i++) {
             int j;
             String buf = array[i];
 
             for (j = i - 1; j >= 0; j--) {
                 if (array[j].compareTo(buf) < 0) {
+                    break;
+                }
+                array[j + 1] = array[j];
+            }
+
+            array[j + 1] = buf;
+        }
+        return array;
+    }
+
+    public String[] insertionSortDecrease(String[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int j;
+            String buf = array[i];
+
+            for (j = i - 1; j >= 0; j--) {
+                if (array[j].compareTo(buf) > 0) {
                     break;
                 }
                 array[j + 1] = array[j];
