6a99d854c8e0d93a9419a8718eec09ec0803ea22
Just Benya <justbenya@gmail.com>
2017-08-13 00:17:19 +0700

    Добавление сеттеров

--- src/main/java/com/practice/File.java
+++ src/main/java/com/practice/File.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 public class File {
     private ArrayList<Integer> listInt = new ArrayList<>();
     private ArrayList<String> listStr = new ArrayList<>();
+    private java.io.File inputFileName;
+    private java.io.File outputFileName;
 
     private static final java.io.File INPUT_FILE = new java.io.File("in.txt");
     private static final java.io.File OUTPUT_FILE = new java.io.File("out.txt");
@@ -18,12 +20,20 @@ public class File {
         return listStr;
     }
 
+    public void setInputFileName(String inputFileName) {
+        this.inputFileName = new java.io.File(inputFileName);
+    }
+
+    public void setOutputFileName (String outputFileName) {
+        this.outputFileName = new java.io.File(outputFileName);
+    }
+
     /**
      * Считываем информацию с файла.
      * Помещаем её в listInt.
      */
     public void load() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 if (isNumber(line)) {
@@ -33,6 +43,7 @@ public class File {
                 }
             }
         } catch (IOException e) {
+            System.out.println("Error input!");
             e.printStackTrace();
         }
     }
